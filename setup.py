4bf4df2517f66266893308f21b864cef5d8736e3
17674707037 <Wang20000128>
2022-06-23 17:11:38 +0800

    V1.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='sanic-rest-framework',
-    version='1.2.2',
+    version='1.2.3',
     packages=find_packages(),
     description="DOC...",
     author="Tioit-Wang",
